1a63cbb3df00c443fc4b7c39e3d3b7e91aeb3f36
Hamunii <samuel.suni04@gmail.com>
2025-05-22 09:51:22 +0300

    remove unused MonoDetourAnalyzeAttribute

--- src/MonoDetour/MonoDetourHookInitAttribute.cs
+++ src/MonoDetour/MonoDetourHookInitAttribute.cs
@@ -6,11 +6,7 @@ namespace MonoDetour;
 /// Specifies that MonoDetour will call this method if the type it is in is
 /// marked with <see cref="MonoDetourTargetsAttribute"/> and
 /// <see cref="MonoDetourManager.InvokeHookInitializers(System.Reflection.Assembly)"/>
-/// is called.<br/>
-/// <br/>
-/// MonoDetour's HookGen will also analyze this method for HookGen usage
-/// (On.Namespace.Type.Method...) just like <see cref="MonoDetourHookAnalyzeAttribute"/>
-/// so that it can generate the hook methods for those hooks.
+/// is called.
 /// </summary>
 [AttributeUsage(AttributeTargets.Method, Inherited = false)]
 public class MonoDetourHookInitAttribute : Attribute { }
