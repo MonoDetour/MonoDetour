4b19f6a1f5638e9938dca8a65eb010be37d16b52
Hamunii <samuel.suni04@gmail.com>
2025-05-25 23:52:59 +0300

    add wip test for speakable enumerator

---
+++ tests/MonoDetour.UnitTests/FunctionalityTests/SpeakableEnumeratorTests.cs
@@ -0,0 +1,33 @@
+using MonoDetour.Reflection.Unspeakable;
+
+namespace MonoDetour.UnitTests.FunctionalityTests;
+
+public class SpeakableEnumeratorTests
+{
+    public int Number { get; set; } = 0;
+
+    [Fact]
+    void CanUseSpeakableEnumerator()
+    {
+        var stateMachineTarget = ((Delegate)GetEnumerator).Method.GetStateMachineTarget()!;
+
+        using var m = DefaultMonoDetourManager.New();
+        m.Hook<PrefixDetour>(stateMachineTarget, Prefix);
+
+        GetEnumerator().MoveNext();
+
+        Assert.Equal(2, Number);
+    }
+
+    static void Prefix(IEnumerator<int> enumerator)
+    {
+        var self = SpeakableEnumerator<int, SpeakableEnumeratorTests>.GetOrCreate(enumerator);
+        self.DeclaringThis.Number++;
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        Number++;
+        yield break;
+    }
+}
