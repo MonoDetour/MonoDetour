5bfc948d9a4d16253775100585d229edfe28c3a8
Hamunii <samuel.suni04@gmail.com>
2025-05-25 18:40:34 +0300

    add warning about modifying control flow

--- src/MonoDetour/DetourTypes/ReturnFlow.cs
+++ src/MonoDetour/DetourTypes/ReturnFlow.cs
@@ -3,6 +3,16 @@ namespace MonoDetour.DetourTypes;
 /// <summary>
 /// Specifies how the return value affects control flow.
 /// </summary>
+/// <remarks>
+/// WARNING: Modifying the control flow of a method in a hook should NOT
+/// be done if you are copy pasting logic from the original method.
+/// This will <i>destroy</i> compatibility with ILHooks applied to the target method.
+/// In such a case, ALWAYS use an <see cref="ILHookDetour"/> to modify the
+/// CIL instructions of the method directly.<br/>
+/// <br/>
+/// To learn more about ILHooking, see
+/// https://monodetour.github.io/ilhooking/introduction/
+/// </remarks>
 public enum ReturnFlow
 {
     /// <summary>
@@ -15,11 +25,13 @@ public enum ReturnFlow
     /// but doesn't skip prefixes and postfixes written to it.<br/>
     /// This is essentially the same as prefix return false in HarmonyX.
     /// </summary>
+    /// <inheritdoc cref="ReturnFlow"/>
     SkipOriginal = 1,
 
     /// <summary>
     /// Returns immediately and doesn't run any
     /// hooks written to the method.
     /// </summary>
+    /// <inheritdoc cref="ReturnFlow"/>
     HardReturn = 2,
 }
