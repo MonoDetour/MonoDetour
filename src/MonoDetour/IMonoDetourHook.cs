7c6902c9ccc5bead70e9f6369c11a3e6f529510c
Hamunii <samuel.suni04@gmail.com>
2025-05-28 23:29:58 +0300

    remove Applier from IMonoDetourHook
    it is not needed at all right now and maybe MonoDetour doesn't *need* to be limited to only ILHooks

--- src/MonoDetour/IMonoDetourHook.cs
+++ src/MonoDetour/IMonoDetourHook.cs
@@ -10,18 +10,12 @@ namespace MonoDetour;
 public interface IMonoDetourHook : IReadOnlyMonoDetourHook, IDisposable
 {
     /// <summary>
-    /// The <see cref="ILHook"/> that applies the <see cref="IReadOnlyMonoDetourHook.Manipulator"/> method.<br/>
-    /// An applier comes from a class implementing <see cref="IMonoDetourHookApplier"/>.
-    /// </summary>
-    public ILHook Applier { get; }
-
-    /// <summary>
-    /// Applies the <see cref="Applier"/> if it was not already applied.
+    /// Applies the hook if it was not already applied.
     /// </summary>
     public void Apply();
 
     /// <summary>
-    /// Undoes the <see cref="Applier"/> if it was applied.
+    /// Undoes the hook if it was applied.
     /// </summary>
     public void Undo();
 }
